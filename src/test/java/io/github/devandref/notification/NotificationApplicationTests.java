c66b7f1ee5d7da3e60588c0d2f766927b7a68db4
devandref <yoandredepaula@gmail.com>
2024-07-29 20:03:31 -0300

    init

---
+++ src/test/java/io/github/devandref/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
